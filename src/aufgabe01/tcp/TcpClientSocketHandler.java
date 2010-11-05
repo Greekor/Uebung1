a4d954648aa9222311916d1f3efeed6e8aaf0369
Greekor <greekor@googlemail.com>
2010-11-05 18:03:37 +0100

    Aufgabe 1.3 TcpClientSocketHandler
    Done!

--- src/aufgabe01/tcp/TcpClientSocketHandler.java
+++ src/aufgabe01/tcp/TcpClientSocketHandler.java
@@ -2,19 +2,39 @@ package aufgabe01.tcp;
 
 import java.io.IOException; 
 import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.Socket;
 
 import aufgabe01.ClientSocketHandler;
 
 public final class TcpClientSocketHandler implements ClientSocketHandler {
 
+	private boolean lastReceived = true;
+	private Socket s;
+	
 	@Override
-	public Serializable receive() throws IOException {
+	public Serializable receive() throws IOException, IllegalStateException {
 		// TODO: implement //
-		return null;
+		if(lastReceived) throw new IllegalStateException();
+		lastReceived = true;
+
+		Serializable sl = TcpHelper.readObjectFromSocket(s);
+		
+		s.close();
+		
+		return sl;
 	}
 
 	@Override
-	public void send(String tmp) throws IOException {
+	public void send(String tmp) throws IOException, IllegalStateException {
 		// TODO: implement //
+		if(!lastReceived) throw new IllegalStateException();
+		lastReceived = false;
+		
+		InetAddress iaddr = InetAddress.getByName("SERVER");
+		s = new Socket(iaddr, 30303);
+		
+		TcpHelper.writeDataToSocket(s, tmp);
+		
 	}
 }
