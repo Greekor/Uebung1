4dfdb71546f67e60a5bfbb6b7682cc8458bbc3d9
Greekor <greekor@googlemail.com>
2010-11-05 17:35:42 +0100

    Aufgabe 1.3 TCPHelper
    TODO: keine Instanz

--- src/aufgabe01/tcp/TcpHelper.java
+++ src/aufgabe01/tcp/TcpHelper.java
@@ -1,18 +1,27 @@
 package aufgabe01.tcp;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
 
+// TODO: keine Instanzen erzeugen lassen??
 final class TcpHelper {
 
 	static void writeDataToSocket(Socket s, Serializable result) throws IOException {
-		// TODO implement //
+		
+		ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+		oos.writeObject(result);
 	}
 	
 	static Serializable readObjectFromSocket(Socket s) throws IOException {
-		// TODO implement //
-		return null;
+		try {
+			ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+			return (Serializable) ois.readObject();
+		} catch(ClassNotFoundException e) {
+			return null;
+		}
 	}
 	
 }
